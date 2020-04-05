4d26903eef9f8a866dbc3d7da401ff96b6d9dbd8
Jozsef Rideg <ridegjozsef@gmail.com>
2020-04-05 12:03:51 +0200

    Challenge 3 related to arrays and functions

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -459,6 +459,7 @@ console.log(whatDoYouDo('retired', 'Mark'));
 /*****************************
 * Arrays
 */
+/*
 
 // Initialize new array
 var names = ['John', 'Mark', 'Jane'];
@@ -478,6 +479,7 @@ names[names.length] = 'Mary';
 console.log(names);
 
 // Different data types
+console.log('')
 var john = ['John', 'Smith', 1990, 'designer', false];
 console.log(john);
 
@@ -502,3 +504,27 @@ console.log(john.indexOf(23));
 var isDesigner =john.indexOf('designer') === -1 ? 'John is NOT a designer' 
     : 'John IS a designer';
 console.log(isDesigner);
+
+*/
+
+/*****************************
+* CODING CHALLENGE 3
+*/
+
+/*
+John and his family went on a holiday and went to 3 different restaurants. The bills were $124, $48 and $268.
+
+To tip the waiter a fair amount, John created a simple tip calculator (as a function). He likes to tip 20% of the bill when the bill is less than $50, 15% when the bill is between $50 and $200, and 10% if the bill is more than $200.
+
+In the end, John would like to have 2 arrays:
+1) Containing all three tips (one for each bill)
+2) Containing all three final paid amounts (bill + tip).
+
+(NOTE: To calculate 20% of a value, simply multiply it with 20/100 = 0.2)
+
+GOOD LUCK 😀
+*/
+
+/*****************************
+* CODING CHALLENGE 3 - MY OWN SOLUUTION (JOZSEF RIDEG)
+*/
