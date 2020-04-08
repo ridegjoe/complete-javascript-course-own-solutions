9682ade777f27ee7a03ea8e36afced57d5ead9ba
Jozsef Rideg <ridegjozsef@gmail.com>
2020-04-08 14:26:56 +0200

    objects and methods

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -650,6 +650,7 @@ console.log(tips, finalValues);
 * Objects and properties
 */
 
+/*
 // Object literal
 var john = {
     firstName: 'John',
@@ -678,3 +679,63 @@ jane.firstName = 'Jane';
 jane.birthYear = 1969;
 jane['lastName'] = 'Smith';
 console.log(jane);
+*/
+
+/*
+/*****************************
+* Objects and properties
+*/
+
+// Object literal
+/*
+// V1
+var john = {
+    firstName: 'John',
+    lastName: 'Smith',
+    birthYear: 1990,
+    family: ['Jane', 'Mark', 'Emily'],
+    job: 'teacher',
+    isMarried: false,
+    calcAge: function(birthYear) {
+        return 2018 - birthYear;
+    }
+};
+console.log(john.calcAge(1990));
+
+*/
+
+/*
+// V2
+var john = {
+    firstName: 'John',
+    lastName: 'Smith',
+    birthYear: 1992,
+    family: ['Jane', 'Mark', 'Emily'],
+    job: 'teacher',
+    isMarried: false,
+    calcAge: function() {
+        return 2018 - this.birthYear;
+    }
+};
+console.log(john.calcAge());
+*/
+
+// V3
+var john = {
+    firstName: 'John',
+    lastName: 'Smith',
+    birthYear: 1992,
+    family: ['Jane', 'Mark', 'Emily'],
+    job: 'teacher',
+    isMarried: false,
+    calcAge: function() {
+        return 2018 - this.birthYear;
+    }
+};
+
+// var age = john.calcAge();
+// john.age = age;
+
+john.age = john.calcAge();
+
+console.log(john);
