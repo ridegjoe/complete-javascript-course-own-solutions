6024158e7c7f097a7518157b1b35a50b43dcb6fb
Jozsef Rideg <ridegjozsef@gmail.com>
2020-04-14 13:38:39 +0200

    tutorial about loops: for, while, continue, break

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -857,9 +857,11 @@ if (john.calcBMI() > mark.calcBMI()) {
 * Loops and iteration
 */
 
+/*
 for (var i = 0; i <= 20; i += 2) {
     console.log(i);
 }
+*/
 
 // i = 0, 0 < 10 true, log i to console, i++
 // i = 1, 1 < 10 true, log i to console, i++
@@ -867,18 +869,52 @@ for (var i = 0; i <= 20; i += 2) {
 // i = 9, 9 < 10 true, log i to console, i++
 // i = 10, 10 < 10 FALSE, exit the loop!
 
-var john = ['John', 'Smith', 1990, 'designer', false];
+/*
+var john = ['John', 'Smith', 1990, 'designer', false, 'blue'];
 
 console.log('');
+
 // For loop
 for (var i = 0; i < john.length; i++) {
     console.log(john[i]);
 }
 
 console.log('');
+
 // While loop
 var i = 0;
 while(i < john.length) {
     console.log(john[i]);
     i++;
+}
+*/
+
+// continue and break statements
+var john = ['John', 'Smith', 1990, 'designer', false, 'blue'];
+for (var i = 0; i < john.length; i++) {
+    if (typeof john[i] !== 'string') continue;
+    console.log(john[i]);
+}
+
+console.log('');
+
+for (var i = 0; i < john.length; i++) {
+    if (typeof john[i] !== 'string') break;
+    console.log(john[i]);
+}
+
+// mini challenge: iterating in the reverse direction
+// Looping backward - my own solution
+console.log('');
+console.log(john.length)
+console.log('');
+
+for (var i = (john.length-1); i >= 0; i--) {
+    console.log(john[i]);
+}
+
+console.log('');
+// Looping backward - official solution
+for (var i = john.length - 1; i >= 0; i--){
+    console.log(john[i]);
 }
