cdbcda9d83f57bd780ae25fdf6d27d4ee32ba461
Jozsef Rideg <ridegjozsef@gmail.com>
2020-04-14 15:21:30 +0200

    Coding challenge 5 - my own solution part 1

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -890,6 +890,7 @@ while(i < john.length) {
 */
 
 // continue and break statements
+/*
 var john = ['John', 'Smith', 1990, 'designer', false, 'blue'];
 for (var i = 0; i < john.length; i++) {
     if (typeof john[i] !== 'string') continue;
@@ -902,9 +903,11 @@ for (var i = 0; i < john.length; i++) {
     if (typeof john[i] !== 'string') break;
     console.log(john[i]);
 }
+*/
 
 // mini challenge: iterating in the reverse direction
 // Looping backward - my own solution
+/*
 console.log('');
 console.log(john.length)
 console.log('');
@@ -918,3 +921,117 @@ console.log('');
 for (var i = john.length - 1; i >= 0; i--){
     console.log(john[i]);
 }
+*/
+
+/*****************************
+* CODING CHALLENGE 5
+*/
+
+/*
+Remember the tip calculator challenge? Let's create a more advanced version using everything we learned!
+
+This time, John and his family went to 5 different restaurants. The bills were $124, $48, $268, $180 and $42.
+John likes to tip 
+20% of the bill when the bill is less than $50, 
+15% when the bill is between $50 and $200, and 
+10% if the bill is more than $200.
+
+Implement a tip calculator using objects and loops:
+1. Create an object with an array for the bill values
+2. Add a method to calculate the tip
+3. This method should include a loop to iterate over all the paid bills and do the tip calculations
+4. As an output, create 1) a new array containing all tips, and 2) an array containing final paid amounts (bill + tip). 
+HINT: Start with two empty arrays [] as properties and then fill them up in the loop.
+
+
+EXTRA AFTER FINISHING: Mark's family also went on a holiday, going to 4 different restaurants. 
+The bills were $77, $375, $110, and $45.
+Mark likes to tip 
+20% of the bill when the bill is less than $100, 
+10% when the bill is between $100 and $300, and 
+25% if the bill is more than $300 (different than John).
+
+5. Implement the same functionality as before, this time using Mark's tipping rules
+6. Create a function (not a method) to calculate the average of a given array of tips. HINT: Loop over the array, and in each iteration store the current sum in a variable (starting from 0). After you have the sum of the array, divide it by the number of elements in it (that's how you calculate the average)
+7. Calculate the average tip for each family
+8. Log to the console which family paid the highest tips on average
+
+GOOD LUCK 😀
+*/
+
+var johnBills = {
+    bills: [124, 48, 268, 180, 42],  // an array within the object, containing the bills
+    calcTip: function() {
+        for (var i = 0; i < this.bills.length; i++) {
+            if (0 < this.bills[i] && this.bills[i] < 50) {
+                this.tips.push(this.bills[i] * 0.2);
+                this.finalBills.push(this.bills[i] + this.tips[i]);
+                console.log('bill = ' + this.bills[i] + ' tip = ' + this.tips[i] + ' final bill = ' + this.finalBills[i]);
+            } else if (50 <= this.bills[i] && this.bills[i] < 200) {
+                this.tips.push(this.bills[i] * 0.15);
+                this.finalBills.push(this.bills[i] + this.tips[i]);
+                console.log('bill = ' + this.bills[i] + ' tip = ' + this.tips[i] + ' final bill = ' + this.finalBills[i]);
+            } else if (200 <= this.bills[i] ) {
+                this.tips.push(this.bills[i] * 0.10);
+                this.finalBills.push(this.bills[i] + this.tips[i]);
+                console.log('bill = ' + this.bills[i] + ' tip = ' + this.tips[i] + ' final bill = ' + this.finalBills[i]);
+            } else {
+                console.log(this.bills[i] +' is out of valid range');
+            }
+     
+        }  
+    },
+    tips: [],
+    finalBills: []
+};
+
+console.log('');
+console.log(johnBills);
+
+console.log('');
+console.log(johnBills.bills);
+console.log(johnBills.tips);
+console.log(johnBills.finalBills);
+
+console.log('calling johnBills.calcTip() method');
+johnBills.calcTip();
+console.log(johnBills.bills);
+console.log(johnBills.tips);
+console.log(johnBills.finalBills);
+
+var markBills = {
+    bills: [77, 375, 110, 45],  // an array within the object, containing the bills
+    calcTip: function() {
+        for (var i = 0; i < this.bills.length; i++) {
+            if (0 < this.bills[i] && this.bills[i] < 100) {
+                this.tips.push(this.bills[i] * 0.2);
+                this.finalBills.push(this.bills[i] + this.tips[i]);
+                console.log('bill = ' + this.bills[i] + ' tip = ' + this.tips[i] + ' final bill = ' + this.finalBills[i]);
+            } else if (100 <= this.bills[i] && this.bills[i] < 300) {
+                this.tips.push(this.bills[i] * 0.10);
+                this.finalBills.push(this.bills[i] + this.tips[i]);
+                console.log('bill = ' + this.bills[i] + ' tip = ' + this.tips[i] + ' final bill = ' + this.finalBills[i]);
+            } else if (300 <= this.bills[i] ) {
+                this.tips.push(this.bills[i] * 0.25);
+                this.finalBills.push(this.bills[i] + this.tips[i]);
+                console.log('bill = ' + this.bills[i] + ' tip = ' + this.tips[i] + ' final bill = ' + this.finalBills[i]);
+            } else {
+                console.log(this.bills[i] +' is out of valid range');
+            }
+        }    
+    },
+    tips: [],
+    finalBills: []
+};
+
+console.log('');
+console.log(markBills.bills);
+console.log(markBills.tips);
+console.log(markBills.finalBills);
+console.log(markBills);
+console.log('calling markBills.calcTip() method');
+markBills.calcTip();
+console.log(markBills.bills);
+console.log(markBills.tips);
+console.log(markBills.finalBills);
+console.log(markBills);
