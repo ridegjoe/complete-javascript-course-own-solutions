2ca3c5aa3e79810d6b3f4d6d3ed0ee96f6b87efb
Jozsef Rideg <ridegjozsef@gmail.com>
2020-04-16 13:40:22 +0200

    3 - how JS works - explaining hoisting

--- 3-how-JS-works/script.js
+++ 3-how-JS-works/script.js
@@ -1,20 +1,44 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+// I. Functions
+calculateAge(1965);
 
+function calculateAge(year) {
+    console.log(2016 - year);
+}
+/*
+function calculateAge(year) {
+    console.log(2016 - year);
+}
+calculateAge(1999);
+*/
 
+// retirement(1956);
+var retirement = function(year) {
+    console.log(65 - (2016 - year));
+}
 
+/*
+var retirement = function(year) {
+    console.log(65 - (2016 - year));
+}
+retirement(1990);
+*/
 
+// II. Variables
 
+console.log(age);
+var age = 23;
+// console.log(age);
 
-
-
-
-
-
-
-
-
+function foo() {
+    console.log(age);
+    var age = 65;
+    console.log(age);
+}
+foo();
+console.log(age);
 
 
 ///////////////////////////////////////
