b335c9bca7e163153addfbb39e23ca2e3cdeb8a8
Jozsef Rideg <ridegjozsef@gmail.com>
2020-04-23 11:38:10 +0200

    PIG: updating scores and hiding the dice if 1

--- 4-DOM-pig-game/app.js
+++ 4-DOM-pig-game/app.js
@@ -13,7 +13,7 @@ var scores, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 // dice = Math.floor(Math.random() * 6) + 1;
 // console.log(dice);
@@ -48,14 +48,48 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     // 1. Random number
     var dice = Math.floor(Math.random() * 6) + 1; // I declare it here. I don't need it outside
 
+
     // 2. Display the result
     var diceDOM = document.querySelector('.dice');
+
     // document.querySelector('.dice').style.display = 'block';
     diceDOM.style.display = 'block';
     // dice-1.png, dice-2.png, ... dice-6.png
     diceDOM.src = 'dice-' + dice + '.png';
 
     // 3. Update the round score but only IF the rolled number is NOT a 1
+    if (dice !== 1) {
+        // Add score
+        roundScore += dice; // cleaner way
+        // more comlicated way: roundScore = roundScore + dice
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        // Next player
+        // ternary operator
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        // the same conditions with if ... else
+        /*
+        if (activePlayer === 0) {
+            activePlayer =1;
+        } else {
+            activePlayer =0;
+        }
+        */
+        roundScore = 0;
+        
+        // resetting values to zero, if somehone gets 1 and looses.
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        // document.querySelector('.player-0-panel').classList.remove('active');
+        // document.querySelector('.player-1-panel').classList.add('active');
+
+        // with toggle
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        document.querySelector('.dice').style.display = 'none';
+    }
 
 });
 
