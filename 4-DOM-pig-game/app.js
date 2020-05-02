133221739db6c3fb29587855c107999481faced5
Jozsef Rideg <ridegjozsef@gmail.com>
2020-05-02 18:23:11 +0200

    writing hold dice function

--- 4-DOM-pig-game/app.js
+++ 4-DOM-pig-game/app.js
@@ -65,34 +65,74 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         document.querySelector('#current-' + activePlayer).textContent = roundScore;
     } else {
         // Next player
-        // ternary operator
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-        // the same conditions with if ... else
-        /*
-        if (activePlayer === 0) {
-            activePlayer =1;
-        } else {
-            activePlayer =0;
-        }
-        */
-        roundScore = 0;
-        
-        // resetting values to zero, if somehone gets 1 and looses.
-        document.getElementById('current-0').textContent = '0';
-        document.getElementById('current-1').textContent = '0';
-
-        // document.querySelector('.player-0-panel').classList.remove('active');
-        // document.querySelector('.player-1-panel').classList.add('active');
-
-        // with toggle
-        document.querySelector('.player-0-panel').classList.toggle('active');
-        document.querySelector('.player-1-panel').classList.toggle('active');
-
-        document.querySelector('.dice').style.display = 'none';
+        nextPlayer();
     }
 
 });
 
+
+// in HTML:
+// <button class="btn-hold"><i class="ion-ios-download-outline"></i>Hold</button>
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    // Add CURRENT score to GLOBAL score
+    scores[activePlayer] += roundScore;
+
+    // Update the UI (User Interface)
+    // <div class="player-score" id="score-0">43</div>
+    // <div class="player-score" id="score-1">72</div>
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+    // Check if player won the game
+
+    /* my own solution
+    if (scores[activePlayer] >= 100) {
+        console.log("The winner is: player" + (activePlayer+1) + " with " + scores[activePlayer] + " points");
+    } else {
+        // Next player
+        nextPlayer();
+    }
+    end of my own solution */
+
+    // official solution
+    // in html: <div class="player-name" id="name-0">Player 1</div>
+    // in html: <div class="player-name" id="name-1">Player 2</div>
+    if (scores[activePlayer] >= 20) {
+        document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+    } else {
+        // Next player
+        nextPlayer(); 
+    }
+
+    // winner style in css:
+    // .winner { background-color: #f7f7f7; }
+    // .winner .player-name { font-weight: 300; color: #EB4D4D; }
+
+    // in html the entire left or right side
+    // <div class="player-1-panel">
+
+
+});
+
+
+function nextPlayer() {
+    // Next player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+    
+    // resetting values to zero, if someone gets 1 and looses.
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    // with toggle
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    document.querySelector('.dice').style.display = 'none';
+}
 
 
 
